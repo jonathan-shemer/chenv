d8bccad85c4ed8e5aab513373b5047010b044e79
jonathan-shemer <jonathan-shemer@users.noreply.github.com>
2021-08-30 11:49:18 +0300

    fix: new-lines are treated as literals in double-quoted variables. close #6

--- src/chenv/transform.py
+++ src/chenv/transform.py
@@ -9,18 +9,23 @@ from chenv import settings
 from chenv.models.output import Output
 
 
-def ignore(variables: dict) -> dict:
+def _ignore(variables: dict) -> dict:
     """Filter variable keys by unix patterns in `.envignore`."""
     ignores = set(fs.load_lines(settings.ENVIGNORE))
     return {key: value for key, value in variables.items() if not any(fnmatch(key, ignored) for ignored in ignores)}
 
 
-def merge(variables: dict) -> str:
+def _merge(variables: dict) -> dict:
     """Merge / override variable set with env-variables defined in `.envmerge`."""
     overriders = fs.load(path=settings.ENVMERGE)
     return toolz.merge(variables, overriders)
 
 
+def _new_lines(variables: dict) -> dict:
+    r"""Replace new-lines with `\n` literal."""
+    return toolz.valmap(lambda v: v.replace("\n", "\\n"), variables)
+
+
 def apply(output: Output) -> Output:
     """Apply all transformations to the `Output` set."""
-    return replace(output, variables=toolz.pipe(output.variables, ignore, merge))
+    return replace(output, variables=toolz.pipe(output.variables, _ignore, _merge, _new_lines))
